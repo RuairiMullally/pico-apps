f4ad304d1cf5cc57add15f44ced02e86023b47ba
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2025-01-28 12:52:11 +0000

    Lab 1 Begin

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,11 +1,52 @@
+#include "pico/stdlib.h"
+
+
 /**
- * @brief LAB #01 - TEMPLATE
- *        Main entry point for the code.
+  * @brief Cyclically turns on and off a specified LED
+  *        for a specified time.
+  *
+  * @return none
+  */
+void toggle_LED(const uint LED_PIN, const uint LED_DELAY){
+    static bool LED_state = 0;
+
+    // Update LED state
+    LED_state = !LED_state;
+    // Toggle the LED 
+    gpio_put(LED_PIN, LED_state);
+    // Sleep specified time
+    sleep_ms(LED_DELAY);
+
+}
+
+
+/**
+ * @brief EXAMPLE - BLINK_C
+ *        Simple example to initialise the built-in LED on
+ *        the Raspberry Pi Pico and then flash it forever. 
  * 
- * @return int      Returns exit-status zero on completion.
+ * @return int  Application return code (zero for success).
  */
 int main() {
 
-    // Returning zero indicates everything went okay.
+    // Specify the PIN number and sleep delay
+    const uint LED_PIN   =  25;
+    const uint LED_DELAY = 500;
+
+    // Setup the LED pin as an output.
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Infinitely toggle LED.
+    while (true) {
+      toggle_LED(LED_PIN, LED_DELAY);
+    }
+    
+    // Should never get here due to infinite while-loop.
     return 0;
+
 }
+
+
+
+
