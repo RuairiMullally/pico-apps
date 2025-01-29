d71c811f12e203580d3283d354c16c214b46c99b
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2025-01-29 17:29:47 +0000

    Lab01 Toggle LED update.

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -2,19 +2,17 @@
 
 
 /**
- * @brief Cyclically turn on and off specified LED for
- *        a specified time.
+ * @brief Toggle a specified LED for a specified time.
  * 
  * @param LED_PIN 
  * @param LED_DELAY 
  */
 void toggle_LED(const uint LED_PIN, const uint LED_DELAY){
-    static bool LED_state = 0;
-
-    // Update LED state
-    LED_state = !LED_state;
+    
+    //get current LED state
+    bool LED_state = gpio_get(LED_PIN);
     // Toggle the LED 
-    gpio_put(LED_PIN, LED_state);
+    gpio_put(LED_PIN, !LED_state);
     // Sleep specified time
     sleep_ms(LED_DELAY);
 
