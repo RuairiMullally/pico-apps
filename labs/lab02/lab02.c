bc1653dcb4743d84608d7d1aae5fa1c9d6f32e06
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2025-02-11 13:13:05 +0000

    Init Lab02

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -5,6 +5,12 @@
 #include "pico/float.h"     // Required for using single-precision variables.
 #include "pico/double.h"    // Required for using double-precision variables.
 
+//contants
+#define WALLIS_ITERATIONS 1000000
+#define PI 3.14159265359
+
+float wallisProductFloat(int iterations);
+double wallisProductDouble(int iterations);
 
 /**
  * @brief LAB #02 - TEMPLATE
@@ -13,7 +19,45 @@
  * @return int      Returns exit-status zero on completion.
  */
 int main() {
+    #ifndef WOKWI
+    // Initialise the IO as we will be using the UART
+    // Only required for hardware and not needed for Wokwi
+    stdio_init_all();
+#endif
+    float pi_float = wallisProductFloat(WALLIS_ITERATIONS);
+    double pi_double = wallisProductDouble(WALLIS_ITERATIONS);
+
+    // Print a console message to inform user what's going on.
+    printf("Results:\n");
+    printf("Float Precision PI: %f\n", pi_float);
+    printf("Float Error: %f\n\n", (pi_float - PI)/PI * 100);
+    printf("Double Precision PI: %lf\n", pi_double);
+    printf("Double Error: %lf\n", (pi_double - PI)/PI * 100);
 
     // Returning zero indicates everything went okay.
     return 0;
+}
+
+
+
+float wallisProductFloat(int iterations){
+    float pi_float = 1;
+
+    for(int i = 1; i <= iterations; i++){
+        pi_float = pi_float * (4 * i * i) / (4 * i * i - 1);
+        //printf("%f", pi_float);
+    }
+    
+    return pi_float * 2;
+}
+
+
+double wallisProductDouble(int iterations){
+    double pi_double = 1;
+
+    for(int i = 1; i <= iterations; i++){
+        pi_double = pi_double * (4 * i * i) / (4 * i * i - 1);
+    }
+    
+    return pi_double * 2;
 }
