395fb1e33dc774d6a125fc64aefc2471fdd04c4c
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2025-02-13 11:00:21 +0000

    Fix Wallis floating point problems.

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -5,12 +5,17 @@
 #include "pico/float.h"     // Required for using single-precision variables.
 #include "pico/double.h"    // Required for using double-precision variables.
 
-//contants
-#define WALLIS_ITERATIONS 1000000
+//constants
+#define WALLIS_ITERATIONS 100000
 #define PI 3.14159265359
 
-float wallisProductFloat(int iterations);
-double wallisProductDouble(int iterations);
+const float PI_F = 3.14159265359f;
+const double PI_D = 3.14159265359;
+const float WALLIS_F = 100000.0f;
+const double WALLIS_D = 100000.0; 
+
+float wallisProductFloat(float iterations);
+double wallisProductDouble(double iterations);
 
 /**
  * @brief LAB #02
@@ -24,17 +29,17 @@ int main() {
     #ifndef WOKWI
     // Initialise the IO as we will be using the UART
     // Only required for hardware and not needed for Wokwi
-    stdio_init_all();
-#endif
-    float pi_float = wallisProductFloat(WALLIS_ITERATIONS);
-    double pi_double = wallisProductDouble(WALLIS_ITERATIONS);
+        stdio_init_all();
+    #endif
+    float pi_float = wallisProductFloat(WALLIS_F);
+    double pi_double = wallisProductDouble(WALLIS_D);
 
     // Print a console message to inform user what's going on.
     printf("Results:\n");
     printf("Float Precision PI: %f\n", pi_float);
-    printf("Float Error: %f\n\n", (pi_float - PI)/PI * 100);
+    printf("Float Error: %f%%\n\n", (pi_float - PI_F)/PI_F * 100);
     printf("Double Precision PI: %lf\n", pi_double);
-    printf("Double Error: %lf\n", (pi_double - PI)/PI * 100);
+    printf("Double Error: %lf%%\n", (pi_double - PI_D)/PI_D * 100);
 
     // Returning zero indicates everything went okay.
     return 0;
@@ -45,12 +50,12 @@ int main() {
  * @brief Calculates the Wallis product using a _float_ for i iterations.
  * 
  * @param iterations 
- * @return float 
+ * @return float PI calculation
  */
-float wallisProductFloat(int iterations){
-    float pi_float = 1;
+float wallisProductFloat(float iterations){
+    float pi_float = 1.0f;
 
-    for(int i = 1; i <= iterations; i++){
+    for(float i = 1.0f; i <= iterations; i++){
         pi_float = pi_float * (4 * i * i) / (4 * i * i - 1);
         //printf("%f", pi_float);
     }
@@ -62,12 +67,12 @@ float wallisProductFloat(int iterations){
  * @brief Calculates the Wallis product using a _double_ for i iterations.
  * 
  * @param iterations 
- * @return double 
+ * @return double PI calculation
  */
-double wallisProductDouble(int iterations){
-    double pi_double = 1;
+double wallisProductDouble(double iterations){
+    double pi_double = 1.0;
 
-    for(int i = 1; i <= iterations; i++){
+    for(double i = 1.0; i <= iterations; i++){
         pi_double = pi_double * (4 * i * i) / (4 * i * i - 1);
     }
     
